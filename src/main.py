34a54a707d9b2ca8840337e6a33d5e4cbe757b89
Alex <alexander.t.rorich@gmail.com>
2024-11-28 16:45:13 +0200

    Update from course

--- src/main.py
+++ src/main.py
@@ -1,27 +1,20 @@
 import os
 import shutil
 
-def copy_static(src: str, dst: str):
-    # Remove existing destination if it exists
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    
-    # Create destination directory
-    os.mkdir(dst)
-    
-    # Copy files recursively
-    for item in os.listdir(src):
-        src_path = os.path.join(src, item)
-        dst_path = os.path.join(dst, item)
-        
-        if os.path.isfile(src_path):
-            shutil.copy(src_path, dst_path)
-            print(f"Copied {src_path} to {dst_path}")
-        else:
-            copy_static(src_path, dst_path)
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    copy_static("static", "public")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-if __name__ == "__main__":
-    main()
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
